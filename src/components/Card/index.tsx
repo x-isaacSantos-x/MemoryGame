ba89c1c9de66836dc08ba810ef886f1b7fc215be
Isaac Mendes <isaac.misionero18@gmail.com>
2025-11-07 15:44:59 -0300

    Create: Memory Game using React + TP + VITE

---
+++ src/components/Card/index.tsx
@@ -0,0 +1,17 @@
+import './styles.css';
+
+
+function Card () {
+    
+    return <div className="card">
+
+        <div className="card__contend"></div>
+        <div className="card__face">Front</div>
+        <div className="card__face">Back</div>
+
+    </div>
+
+
+}
+
+export default Card;
---
+++ src/templates/App/index.tsx
@@ -0,0 +1,14 @@
+import Card from '../../components/Card'
+import './styles.css'
+
+function App() {
+ 
+
+  return (
+ 
+    <Card />
+
+  )
+}
+
+export default App
