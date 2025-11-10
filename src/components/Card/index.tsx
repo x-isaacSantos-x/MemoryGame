681739fcb01572c88c71adf926c35e885b9de6cc
Isaac Mendes <isaac.misionero18@gmail.com>
2025-11-10 15:13:59 -0300

    Finish: memory game made

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,17 +1,28 @@
 import './styles.css';
 
-
-function Card () {
-    
-    return <div className="card">
-
-        <div className="card__contend"></div>
-        <div className="card__face">Front</div>
-        <div className="card__face">Back</div>
-
-    </div>
-
-
+export interface CardProps {
+  id: string;
+  flipped?: boolean;
+  back: string;
+  handleClick?: (id: string) => void;
 }
 
-export default Card;
+export function Card({ flipped = false, back, handleClick, id }: CardProps) {
+  const cardContentClassNames = ['card__content'];
+  flipped && cardContentClassNames.push('card__content--flipped');
+
+  const handleClickFn = (id: string) => {
+    if (handleClick) {
+      handleClick(id);
+    }
+  };
+
+  return (
+    <div className="card" onClick={() => handleClickFn(id)}>
+      <div className={cardContentClassNames.join(' ')}>
+        <div className="card__face card__face--front">?</div>
+        <div className="card__face card__face--back">{back}</div>
+      </div>
+    </div>
+  );
+}
--- src/templates/App/index.tsx
+++ src/templates/App/index.tsx
@@ -1,12 +1,22 @@
-import Card from '../../components/Card'
+
+import { Card } from '../../components/Card';
+import { Grid } from '../../components/Grid';
+import { cards } from '../../data/cards';
 import './styles.css'
 
 function App() {
+
+  const handleClick = (id: string) => {
+    console.log(id);
+
+  }
  
 
   return (
  
-    <Card />
+    <div className='App'>
+      <Grid cards={cards} />
+    </div>
 
   )
 }
