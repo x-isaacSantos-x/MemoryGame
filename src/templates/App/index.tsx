a5934d5624701176ad6c3f6242ecdd8cedfa1f72
Isaac Mendes <84507824+x-isaacSantos-x@users.noreply.github.com>
2025-11-10 16:11:44 -0300

    Update index.tsx

--- src/templates/App/index.tsx
+++ src/templates/App/index.tsx
@@ -3,10 +3,12 @@ import { cards } from '../../data/cards';
 import './styles.css';
 
 
-export function App() {
+function App() {
   return (
     <div className="app">
       <Grid cards={cards} />
     </div>
   );
 }
+
+export default App;
